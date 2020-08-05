b9438d08b857d093510aeeb7155e4a8a93c35510
jayanga1996 <52499645+jayanga1996@users.noreply.github.com>
2020-08-05 15:46:17 -0700

    add hello word

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {BrowserRouter,Route,Switch} from 'react-router-dom'
 function App() {
   return (
     <BrowserRouter>
+    <p>Hello</p>
     <switch>
       <Route path='/' component={SignIn} exact/>
     </switch>
