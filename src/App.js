f9e719d57f9788bd52fe2c0c0a5722a6cd2e9b36
jayanga1996 <52499645+jayanga1996@users.noreply.github.com>
2020-08-05 15:25:25 -0700

    I set component folder name same

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {SignIn} from './Component/SignInUI'
+import {SignIn} from './component/SignInUI'
 // eslint-disable-next-line no-unused-vars
 import {BrowserRouter,Route,Switch} from 'react-router-dom'
 
