c4e99e777f9c6a12376fdcbe2c9ce3ef25d5d6fc
Nadeeka-max <you@example.com>
2020-08-07 03:30:08 +0530

    Add UI for the home page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import {BrowserRouter,Route,Switch} from 'react-router-dom'
 import login from './components/login';
 import register from './components/register';
 import profile from './components/profile';
+import Home from './components/home';
 
 function App() {
   return (
     <BrowserRouter>
-    <login/>
+    
     <Switch>
-      <Route path='/' component={login} exact/>
+      <Route path='/' component={Home} exact/>
+      <Route path='/login' component={login} exact/>
       <Route path='/register' component={register} />
       <Route path='/profile' component={profile}/>
     </Switch>
--- src/components/profile.js
+++ src/components/profile.js
@@ -6,10 +6,10 @@ export default function profile(){
             <section class="text-gray-900 body-font bg-pink-100">
                 <header class="text-gray-700 body-font bg-indigo-500">
                     <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                        <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                        
                             
-                            <span class="ml-3 text-xl">Dr.Saman Wejethunga</span>
-                        </a>
+                            <span class="ml-3 text-10xl">Dr.Saman Wejethunga</span>
+                  
                         <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
                             <a class="mr-5 hover:text-gray-900">First Link</a>
                             <a class="mr-5 hover:text-gray-900">Second Link</a>
